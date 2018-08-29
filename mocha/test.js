63e6dc7b0737494b5ae68816bb985d62bfa33831
bottle93 <alestrzelec@gmail.com>
2018-08-29 14:41:04 +0200

    testing with Chai

--- mocha/test.js
+++ mocha/test.js
@@ -1,6 +1,10 @@
 var {Client} = require('../shop.js');
 var assert = require('assert');
+var assertChai = require('chai').assert
+var expect = require('chai').expect
+var should = require('chai').should()
 
+//Pure mocha
 describe('clientDiscount', function () {
   it('should-count-all-discount-combo', function () {
     var client = new Client(16, 'female', [{cost: 25}, {cost: 1005}, {cost: 875}])
@@ -29,5 +33,22 @@ describe('clientDiscount', function () {
   it('should-discount-all-orders-above-1000', function () {
     var client = new Client(36, 'male', [{cost: 2545}, {cost: 15}, {cost: 85}])
     assert.equal(client.getDiscount(29), 10)
+  })
+})
+
+// mocha with Chai
+
+describe('client-discount-chai-testing', function () {
+  it('should-count-all-discount-combo-chai', function () {
+    var client = new Client(16, 'female', [{cost: 25}, {cost: 1005}, {cost: 875}])
+    assertChai.equal(client.getDiscount(234), 23)
+  })
+  it('should-discount-all-orders-above-1000-chai-testing', function () {
+    var client = new Client(36, 'male', [{cost: 2545}, {cost: 15}, {cost: 85}])
+    expect(client.getDiscount(28)).to.equal(10)
+  })
+  it('should-discount-above-200', function () {
+    var client = new Client(36, 'male', [{cost: 25}, {cost: 15}, {cost: 85}])
+    client.getDiscount(290).should.equal(5)
   })
 })
